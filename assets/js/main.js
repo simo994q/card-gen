9fa746b6252a6a1c91cf3449a58a24e0405cc835
simo994q <simonkn1@hotmail.com>
2023-04-28 13:50:58 +0200

    buttons

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,6 @@
 
 
 let boxShadowElems = document.querySelectorAll('#boxShadowDetails input')
-console.log(boxShadowElems);
 
 boxShadowElems.forEach(function (element) {
     element.addEventListener('keyup', function () {
@@ -31,11 +30,64 @@ boxShadowSelector.addEventListener('change', () => {
 })
 
 function boxShadow() {
-
     cardBoxShadow = `${boxShadowElems[0].value}px ${boxShadowElems[1].value}px ${boxShadowElems[2].value}px ${boxShadowElems[3].value}px #${boxShadowElems[4].value}`
 
     card.style.boxShadow = cardBoxShadow
-
-
-
 };
+
+
+
+const paddingSelector = document.getElementById('paddingSelector')
+const paddingDetails = document.getElementById('paddingDetails')
+const paddingValue = document.getElementById('paddingValue')
+
+paddingValue.addEventListener('keyup', function () {
+    card.style.padding = `${paddingValue.value}px`
+})
+
+
+paddingSelector.addEventListener('change', () => {
+    if (paddingSelector.checked == true) {
+        paddingDetails.style.display = "flex"
+        card.style.padding = `${paddingValue.value}px`
+    }
+    else {
+        paddingDetails.style.display = "none"
+        card.style.padding = "0"
+    }
+})
+
+
+
+const addElementButtons = document.querySelectorAll('#addElements button')
+const elementsList = document.getElementById('elementsList')
+
+addElementButtons.forEach(function (element) {
+    element.addEventListener('click', function () {
+        elementsList.innerHTML += `<li>${element.innerHTML}</li>`
+        cardInsertSwitch(element.innerHTML)
+    })
+});
+
+function cardInsertSwitch(element) {
+    switch (element) {
+        case 'h1':
+            card.innerHTML += `<h1>h1</h1>`
+            break;
+        case 'h2':
+            card.innerHTML += `<h2>h2</h2>`
+            break;
+        case 'h3':
+            card.innerHTML += `<h3>h3</h3>`
+            break;
+        case 'p':
+            card.innerHTML += `<p>p</p>`
+            break;
+        case 'div':
+            card.innerHTML += `<div>div</div>`
+            break;
+        case 'img':
+            card.innerHTML += `<img src="assets/img/placeholder.jpg">`
+            break;
+    }
+}
